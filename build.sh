c471b37639e36c0456da0c911acbdd731761806b
Carles Pina i Estany <carles@pina.cat>
2022-09-01 22:49:58 +0100

    Add step to filter certain files

--- build.sh
+++ build.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-mkdir output
+# This is an example of some "process". Here it uses
+# pandoc to convert from MarkDown to different formats
+# and saves the output into "output_temp"
 
-pandoc -s main.md -o output/main.html
-pandoc main.md -o output/main_bare.html
-pandoc -s main.md -o output/main.pdf
-pandoc -s main.md -o output/main.epub
-pandoc -s main.md -o output/main.odt
-pandoc -s main.md -o output/main.docx
+OUTPUT_DIR="output_temp"
 
-date > output/.build_date.txt
+mkdir "$OUTPUT_DIR"
+
+pandoc -s main.md -o "$OUTPUT_DIR"/main.html
+pandoc main.md -o "$OUTPUT_DIR"/main_bare.html
+pandoc -s main.md -o "$OUTPUT_DIR"/main.pdf
+pandoc -s main.md -o "$OUTPUT_DIR"/main.epub
+pandoc -s main.md -o "$OUTPUT_DIR"/main.odt
+pandoc -s main.md -o "$OUTPUT_DIR"/main.docx
+
+date > "$OUTPUT_DIR"/.build_date.txt
 
 echo "generated_at: $(date)" > variables.yml
 
-mustache variables.yml index.output.html > output/index.html
-mustache variables.yml README.output.md > output/README.md
+mustache variables.yml index."$OUTPUT_DIR".html > "$OUTPUT_DIR"/index.html
+mustache variables.yml README."$OUTPUT_DIR".md > "$OUTPUT_DIR"/README.md
 
